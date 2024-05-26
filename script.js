951f21c69d68b1d897fde3e9e83600f8457f8268
Rselva1097 <99159175+Rselva1097@users.noreply.github.com>
2024-05-26 00:22:00 -0400

    created a submission

--- script.js
+++ script.js
@@ -1,2 +1,18 @@
-//your code here
+document.getElementById('swap').addEventListener('click', swapTheme);
 
+function swapTheme() {
+    const app = document.getElementById('app');
+    const button = document.getElementById('swap');
+
+    if (app.classList.contains('day')) {
+        app.classList.remove('day');
+        app.classList.add('night');
+        button.classList.remove('button_day');
+        button.classList.add('button_night');
+    } else {
+        app.classList.remove('night');
+        app.classList.add('day');
+        button.classList.remove('button_night');
+        button.classList.add('button_day');
+    }
+}
